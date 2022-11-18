57f71b7b10605c7058eaec107d46d8933ef3ecb3
Jbristhuille <jbristhuille@gmail.com>
2022-11-18 15:43:49 +0100

    :memo: Add story context to "ex1_isAlive"

--- 02_corrections/ex1_isAlive/ex1_isAlive.js
+++ 02_corrections/ex1_isAlive/ex1_isAlive.js
@@ -1,4 +1,6 @@
 /*
+Un aventurier est retrouvé inconscient, un médecin essaie de déterminer si celui-ci est toujours en vie.
+
 Créer une fonction 'isAlive' qui va vérifier si un joueur est vivant ou mort.
 Si le joueur est vivant la fonction doit retourner 'Is alive with ... HP' (... étant remplacé par le nombre de points de vie)
 Sinon, le joueur est mort et la fonction doit retourner 'Player is dead !'
